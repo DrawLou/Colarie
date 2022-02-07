e4f2a2224bf6c96095b420e25a9e838c450a9e5a
DrawLou <74010593+DrawLou@users.noreply.github.com>
2022-02-07 20:21:57 +0100

    add chartjs line and pie

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // On déclare les datas :
 // le label + sa donnée
 // On peut configurer aussi l'apparance
-const data = {
+const data1 = {
     labels: ["Insee 1980", "Insee 1991", "ObÉpi 1997", "ObÉpi 2000", "ObÉpi 2003", "ObÉpi 2006", "ObÉpi 2009", "ObÉpi 2012", "ObÉpi 2021"],
     datasets: [{
         label: "Pourcentage d'obésité en France ( + de 18ans)",
@@ -14,17 +14,45 @@ const data = {
 };
 
 // On configure notre chart
-const config = {
+const config1 = {
     type: 'line',
-    data: data,
+    data: data1,
     options: {
         aspectRatio: 3, // Pour le responsive
-        
     }
 };
 
 // On affiche le chart dans l'élément avec l'ID correspondant
-var myChart = new Chart(
-    document.getElementById('myChart'),
-    config
+var myChart1 = new Chart(
+    document.getElementById('myChart1'),
+    config1
+);
+
+const data2 = {
+    labels: [
+      "Normal",
+      "Surpoid",
+      "Obésité",
+      "Obésité sévère"
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [52.7, 30.3, 17, 2],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+      ],
+      hoverOffset: 4
+    }]
+  };
+
+  const config2 = {
+    type: 'pie',
+    data: data2,
+  };
+
+  var myChart2 = new Chart(
+    document.getElementById('myChart2'),
+    config2
 );
