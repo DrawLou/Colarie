533c7d27ec993d237ee756ca4f83df9c33619293
DrawLou <74010593+DrawLou@users.noreply.github.com>
2022-02-08 21:34:40 +0100

    delete slide vos avis

--- script.js
+++ script.js
@@ -89,34 +89,3 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
-
-// SLIDE2
-
-var slideIndex2 = 1;
-showSlides2(slideIndex2);
-
-// Next/previous controls
-function plusSlides2(n) {
-  showSlides2(slideIndex2 += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides2(slideIndex2 = n);
-}
-
-function showSlides2(n) {
-  var i;
-  var slides2 = document.getElementsByClassName("mySlides2");
-  var dots2 = document.getElementsByClassName("dot2");
-  if (n > slides2.length) {slideIndex2 = 1}
-  if (n < 1) {slideIndex2 = slides2.length}
-  for (i = 0; i < slides2.length; i++) {
-      slides2[i].style.display = "none";
-  }
-  for (i = 0; i < dots2.length; i++) {
-      dots2[i].className = dots2[i].className.replace(" active2", "");
-  }
-  slides2[slideIndex2-1].style.display = "block2";
-  dots2[slideIndex2-1].className += " active2";
-}
